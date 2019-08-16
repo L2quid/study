0aa9fbc705f1551c8ff924e81a2764db1bbed583
L2quid <wodnd101@naver.com>
2019-08-16 23:58:38 +0900

    네이버 음악순위
    네이버 음악순위 추가, txt저장 인코딩방식 변경

---
+++ python/네이버음악 순위.py
@@ -0,0 +1,12 @@
+from bs4 import BeautifulSoup
+import urllib.request
+
+url = 'https://music.naver.com/'
+
+with urllib.request.urlopen(url) as fs :
+    soup = BeautifulSoup(fs.read().decode(fs.headers.get_content_charset()), 'html.parser')
+    items = soup.find_all('span', {'class' : 'm_ell'})
+
+for i in range(10) :
+    print(str(i + 1) + '위\t: ' + items[i*3].get_text(strip = True)+ '\t' + items[i*3+1].get_text(strip = True))
+input()
--- python/리디북스 순위.py
+++ python/리디북스 순위.py
@@ -13,6 +13,6 @@ local = datetime.datetime.now()
 date= str(local.year)+'_'+str(local.month)+'_'+str(local.day)
 desktopPath = os.path.expanduser('~')
 filePath = desktopPath + '\desktop\리디북스 순위\리디북스'+str(date)+'.txt'
-with open( filePath, 'w+') as file:
+with open( filePath, 'w+',encoding='utf-8') as file:
     for i in range(20) :
         file.write(str(i + 1) + '위\t ' + items[i+1].get_text(strip = True)+'-'+name[i+1].get_text(strip = True)+'\t'+price[i+1].get_text(strip = True)+'\n')
