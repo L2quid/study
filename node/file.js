f91ae2067e88ee75a4b5e03d145c40f1f9ad819e
QuiD <wodnd101@naver.com>
2020-08-03 23:56:39 +0900

    test
    test code

--- node/file.js
+++ node/file.js
@@ -1,5 +1,13 @@
 var fs = require('fs');
-fs.readFile('sample.txt','utf8',function(err,data)
-           {
+
+fs.readFile('sample.txt','utf8',function(err,data){
     console.log(data);
-})
+});
+
+var testFolder = './data';
+
+fs.readdir(testFolder, function(error, filelist){
+    console.log(filelist);
+});
+console.log(__filename);
+console.log(__dirname);
