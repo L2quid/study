5ca72965aeb5a3b5eb823e8a90cd2f2e5fb8443f
QuiD-0 <wodnd101@naver.com>
2021-05-09 00:36:39 +0900

    react timer

--- react/my-app/src/index.js
+++ react/my-app/src/index.js
@@ -4,22 +4,22 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-function returnName(name) {
-  return name.first + " " + name.last;
+function tick(){
+  const ele= (
+    <div>
+      <h1>hello world!</h1>
+      <div> it is {new Date().toLocaleTimeString()} </div>
+    </div>
+  );
+
+  ReactDOM.render(
+    ele,
+    document.getElementById('root')
+  );
 }
 
-const name={
-  first:"jae",
-  last:"ung"
-}
-const ele= <h1>hello {returnName(name)}!</h1>
-
-ReactDOM.render(
-  ele,
-  document.getElementById('root')
-);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+setInterval(tick,1000);
